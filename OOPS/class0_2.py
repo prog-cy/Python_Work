58c8032bfd7bb604d0c143f9c81021cf5691037c
rupeshdwivedi3945@gmail.com <rupeshdwivedi3945@gmail.com>
2021-08-31 14:26:20 +0530

    New folder add

---
+++ OOPS/class0_2.py
@@ -0,0 +1,23 @@
+#creating class and also declaring some method inside the class
+class DemoClass:
+  name = "Rupesh" #class attribute
+  age = 20
+  address = "Pure parson dubey"
+
+
+ob1 = DemoClass()
+ob2 = DemoClass()
+
+print(ob1.name)
+print(ob2.name)
+ob2.name = "Radhika" #instance attribute
+'''
+instance attribute "this is personal property of ob2 not the property of class"
+this will not affect the class name variable
+'''
+print(ob2.name)
+print(ob1.name)
+
+DemoClass.name = "Joy"
+print(ob1.name)
+print(ob2.name)
